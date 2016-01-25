a8c22a43d871a1669fd2be8b05231e4cf52fdb60
jia790 <chenj2@oregonstate.edu>
2016-01-24 20:15:21 -0800

    updated ish

--- Looking4House.php
+++ Looking4House.php
@@ -6,23 +6,18 @@
 
 		<center>
 		
-		<h1 style="color:blue">Housing Application</h1>
+		<h1 style="color:blue">Searching for Housing Application</h1>
 		
 		<div align = "center">
 		
 			<div style = "display: inline-block; text-align: left">
 			
-				<b><p style="font-size: 18px" > You are looking for a ... </p></b>
-				<input type="checkbox" name="mychk[]" value="1"> House <input type="checkbox" name="mychk[]" value="2"> Apartment<br>
+				<b><p style="font-size: 18px" > Type ofhousing  </p></b>
+				<input type="checkbox" name="mychk[]" value="1"> House <input type="checkbox" name="mychk[]" value="2"> Apartment <input type="check" name="mychk[]" value="3">Duplex<br>
 				
-				<br><b>Price: <input type="number" name="price"></b><br>
+				<br><b>Price Range: <input type="number" name="price"></b><br>
 				
-				<b><p style="font-size: 18px" > Relationship Status: </p></b>
-				<input type="radio" name="status" value="1" style="vertical-align: middle"> Single
-				<input type="radio" name="status" value="2" style="vertical-align: middle"> In a relationship
-				<input type="radio" name="status" value="3" style="vertical-align: middle"> Prefer not to answer <br>
-				
-				<br><b>Bed Time: <input type="text" name="bedtime"></b><br>  
+				<br><b>My Bed Time: <input type="text" name="bedtime"></b><br>  
 				
 				<b><p style="font-size: 18px" > Pet(s): </p></b>
 				<input type="radio" name="pettype" value="1" style="vertical-align: middle"> Yes
@@ -30,7 +25,7 @@
 				<input type="radio" name="pettype" value="3" style="vertical-align: middle"> Prefer not to answer <br>
 				 <!--  Type of pet: <input type="text" name="pet"> -->
 				
-				<br><b>Transportation: <input type="text" name="transport"></b><br>
+				<br><b>My Transportation: <input type="text" name="transport"></b><br>
 				
 				
 				<br><input type="submit" value = "Submit" >
