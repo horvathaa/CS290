586ce99912e08eec003a0a7ea7d1403e7313bc87
Taycott <taylor22211@gmail.com>
2016-01-25 18:34:25 -0800

    Add HousingTemplate and house image

---
+++ HousingTemplate.php
@@ -0,0 +1,83 @@
+<html lang = "en">
+<head>
+	<meta charset = "utf-8">
+	<meta name = "viewport" content = "width = device-width, initial-scale = 1">
+	<title> Website Title </title>
+	<link href = "style.css" rel = "stylesheet">
+	<link href="images/logo.ico" rel="shortcut icon">
+</head>
+
+<body>
+<div id = "wrapper">
+	<!-- MAIN NAV ================================================================================-->
+	<div id = "mainNav">
+		<nav>
+			<ul>
+				<li><a href = "index.php"> Home </a></li>
+				<li><a href = "fileName.php"> Other link 1 </a></li>
+				<li><a href = "fileName.php"> Other link 2 </a></li>
+				<li><a href = "profile.php"> Profile </a></li>
+			</ul>
+		</nav>
+	</div>
+	<!-- SLIDESHOW ===============================================================================-->
+	<div id = "slideshow">
+		<a><img id = "slide1" src = "images/house1.png"></a>
+	</div>
+	
+	<div id = "buttons">
+		<p>SLIDE BUTTONS HERE (CSS)</p>
+		<a id = "button1"></a>
+		<a id = "button2"></a>
+		<a id = "button3"></a>
+	</div>
+	
+	<!-- HOUSE NAV ===============================================================================-->
+	<div id "houseDetails">
+		<h3> House Details </h3>
+		<table>
+			<tr>
+				<td> Rent </td>
+				<td> $600 </td>
+			</tr>
+			<tr>
+				<td> Address </td>
+				<td> 1234 NS Stdeet Blvd. </td>
+			</tr>
+			<tr>
+				<td> Bedrooms </td>
+				<td> 4 </td>
+			</tr>
+			<tr>
+				<td> Bathrooms </td>
+				<td> 2 </td>
+			</tr>
+		</table>
+	</div>
+	
+	<!-- HOUSE NAV ===============================================================================-->
+	<div id = "occupantList">
+		<h3> Occupants </h3>
+		<nav>
+			<ul>
+				<li><a href = "wallace.php"> Wallace McCircus </a></li>
+				<li><a href = "alice.php"> Alice Ninnymuggins </a></li>
+				<li><a href = "Schmidt.php"> Schmidt Waterwater </a></li>
+			</ul>
+		</nav>
+	</div>
+	
+	<!-- DESCRIPTION =============================================================================-->
+	<div id = "description">
+		<h3> House Description </h3>
+		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rhoncus at nunc a pharetra. Proin tincidunt mi eget blandit efficitur. Suspendisse a odio efficitur, faucibus purus vitae, mollis nisi. Duis quis sem consectetur, scelerisque nisl at, fringilla mauris. Nullam porttitor posuere sollicitudin. Morbi in blandit dolor. Etiam ut pretium risus. </p>
+		<p> Praesent et nibh facilisis, vulputate quam eu, euismod ipsum. Nulla fringilla risus sed ante dapibus rhoncus. Quisque nec pretium est, vitae blandit tortor. Nulla placerat dolor sit amet suscipit interdum. Praesent varius vehicula ante vitae rutrum. Nulla dolor velit, molestie quis nulla ac, laoreet suscipit nisl. Suspendisse arcu tortor, tempus congue velit at, tincidunt laoreet justo. Suspendisse vitae tortor euismod, elementum nulla quis, lobortis nulla. Nulla blandit elit ac est lacinia elementum. Quisque laoreet risus a tortor finibus sollicitudin. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse quis lacinia nibh. Nam eget aliquam justo. Cras pellentesque felis vitae ultrices porta. Praesent ac orci dui. Phasellus nulla lorem, tristique ornare commodo sed, laoreet consectetur nunc. </p>
+		<p> Mauris diam nisi, gravida non porttitor vulputate, volutpat sed odio. Quisque hendrerit, dui eu mattis pharetra, nisi enim tincidunt elit, id dictum justo arcu in lacus. Proin blandit vehicula purus vitae iaculis. In nec velit nec lectus imperdiet hendrerit. Donec malesuada nisi augue, id cursus metus tristique ut. Fusce leo ante, convallis id lorem facilisis, luctus lacinia tortor. Praesent pulvinar condimentum massa, in iaculis risus fermentum eget. Integer auctor massa in ultrices ullamcorper. Integer eget porttitor orci. Praesent at est ante. Integer et nibh ex. Suspendisse vehicula diam nec ex laoreet consectetur. Curabitur et tempus odio, at laoreet velit. Morbi in feugiat neque. In eget euismod arcu, at lobortis nisl. Ut ut rutrum elit, ac tristique eros. </p>
+	</div>
+	
+</div> 
+
+<script src = "js/slideshow.js"></script>
+
+</body>
+</html>
