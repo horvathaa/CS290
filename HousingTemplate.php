af3e6c4012190ec0d53ece83354c9e2dde309375
Taylor Acott <taylor22211@gmail.com>
2016-02-15 19:03:14 -0800

    Edit CSS and HousingTemplate

--- HousingTemplate.php
+++ HousingTemplate.php
@@ -2,7 +2,7 @@
 <?php include '_header.php' ?>
 
 <!-- SLIDESHOW ===============================================================================-->
-	<div id = "allSlideshow">
+	<div class = "leftCol">
 		<div id = "slideshow">
 			<a><img id = "slide1" src = "images/house1.png"></a>
 			<a><img id = "slide2" src = "images/house2.png"></a>
@@ -17,51 +17,45 @@
 	</div>
 	
 <!-- HOUSE DETAILS ===============================================================================-->
-	<div id = "houseDetails">
-		<h3> House Details </h3>
-		<table>
-			<tr>
-				<td> Rent </td>
-				<td> $600 </td>
-			</tr>
-			<tr>
-				<td> Address </td>
-				<td> 1234 NS Street Blvd. </td>
-			</tr>
-			<tr>
-				<td> Bedrooms </td>
-				<td> 4 </td>
-			</tr>
-			<tr>
-				<td> Bathrooms </td>
-				<td> 2 </td>
-			</tr>
-		</table>
-	</div>
+	<div class = "rightCol">
+		<h3> Details </h3>
+		<div class = "leftText">
+			<p> Rent </p>
+			<p> Address </p>
+			<p> Bedrooms </p>
+			<p> Bathrooms </p>
+			<p> Pets </p>
+		</div>
+		
+		<div class = "rightText">
+			<p> $600 </p>
+			<p> 1234 NS Street Blvd. </p>
+			<p> 4 </p>
+			<p> 2 </p>
+			<p> Allowed </p>
+		</div>
+		
+		<div class = "clearfix"></div>
+		<div class = "break"> </div>
 	
 <!-- OCCUPANT LIST ===============================================================================-->
-	<div id = "occupantList">
 		<h3> Occupants </h3>
-		<ul>
-			<li><a href = "wallace.php"> Wallace McCircus </a></li>
-			<li><a href = "alice.php"> Alice Ninnymuggins </a></li>
-			<li><a href = "Schmidt.php"> Schmidt Waterwater </a></li>
-		</ul>
+		<div class = "centerText">
+			<p><a href = "wallace.php"> Wallace McCircus </a></p>
+			<p><a href = "alice.php"> Alice Ninnymuggins </a></p>
+			<p><a href = "schmidt.php"> Schmidt Waterwater </a></p>
+		</div>
 	</div>
 	
 	<div class = "clearfix"></div>
-	<hr>
+
+	
 <!-- DESCRIPTION =============================================================================-->
-	<div id = "description">
+	<div class = "blockOfText">
 		<h3> House Description </h3>
 		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rhoncus at nunc a pharetra. Proin tincidunt mi eget blandit efficitur. Suspendisse a odio efficitur, faucibus purus vitae, mollis nisi. Duis quis sem consectetur, scelerisque nisl at, fringilla mauris. Nullam porttitor posuere sollicitudin. Morbi in blandit dolor. Etiam ut pretium risus. </p>
 		<p> Praesent et nibh facilisis, vulputate quam eu, euismod ipsum. Nulla fringilla risus sed ante dapibus rhoncus. Quisque nec pretium est, vitae blandit tortor. Nulla placerat dolor sit amet suscipit interdum. Praesent varius vehicula ante vitae rutrum. Nulla dolor velit, molestie quis nulla ac, laoreet suscipit nisl. Suspendisse arcu tortor, tempus congue velit at, tincidunt laoreet justo. Suspendisse vitae tortor euismod, elementum nulla quis, lobortis nulla. Nulla blandit elit ac est lacinia elementum. Quisque laoreet risus a tortor finibus sollicitudin. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse quis lacinia nibh. Nam eget aliquam justo. Cras pellentesque felis vitae ultrices porta. Praesent ac orci dui. Phasellus nulla lorem, tristique ornare commodo sed, laoreet consectetur nunc. </p>
 		<p> Mauris diam nisi, gravida non porttitor vulputate, volutpat sed odio. Quisque hendrerit, dui eu mattis pharetra, nisi enim tincidunt elit, id dictum justo arcu in lacus. Proin blandit vehicula purus vitae iaculis. In nec velit nec lectus imperdiet hendrerit. Donec malesuada nisi augue, id cursus metus tristique ut. Fusce leo ante, convallis id lorem facilisis, luctus lacinia tortor. Praesent pulvinar condimentum massa, in iaculis risus fermentum eget. Integer auctor massa in ultrices ullamcorper. Integer eget porttitor orci. Praesent at est ante. Integer et nibh ex. Suspendisse vehicula diam nec ex laoreet consectetur. Curabitur et tempus odio, at laoreet velit. Morbi in feugiat neque. In eget euismod arcu, at lobortis nisl. Ut ut rutrum elit, ac tristique eros. </p>
-		<p> Mauris diam nisi, gravida non porttitor vulputate, volutpat sed odio. Quisque hendrerit, dui eu mattis pharetra, nisi enim tincidunt elit, id dictum justo arcu in lacus. Proin blandit vehicula purus vitae iaculis. In nec velit nec lectus imperdiet hendrerit. Donec malesuada nisi augue, id cursus metus tristique ut. Fusce leo ante, convallis id lorem facilisis, luctus lacinia tortor. Praesent pulvinar condimentum massa, in iaculis risus fermentum eget. Integer auctor massa in ultrices ullamcorper. Integer eget porttitor orci. Praesent at est ante. Integer et nibh ex. Suspendisse vehicula diam nec ex laoreet consectetur. Curabitur et tempus odio, at laoreet velit. Morbi in feugiat neque. In eget euismod arcu, at lobortis nisl. Ut ut rutrum elit, ac tristique eros. </p>
-		<p> Mauris diam nisi, gravida non porttitor vulputate, volutpat sed odio. Quisque hendrerit, dui eu mattis pharetra, nisi enim tincidunt elit, id dictum justo arcu in lacus. Proin blandit vehicula purus vitae iaculis. In nec velit nec lectus imperdiet hendrerit. Donec malesuada nisi augue, id cursus metus tristique ut. Fusce leo ante, convallis id lorem facilisis, luctus lacinia tortor. Praesent pulvinar condimentum massa, in iaculis risus fermentum eget. Integer auctor massa in ultrices ullamcorper. Integer eget porttitor orci. Praesent at est ante. Integer et nibh ex. Suspendisse vehicula diam nec ex laoreet consectetur. Curabitur et tempus odio, at laoreet velit. Morbi in feugiat neque. In eget euismod arcu, at lobortis nisl. Ut ut rutrum elit, ac tristique eros. </p>
-		<p> Mauris diam nisi, gravida non porttitor vulputate, volutpat sed odio. Quisque hendrerit, dui eu mattis pharetra, nisi enim tincidunt elit, id dictum justo arcu in lacus. Proin blandit vehicula purus vitae iaculis. In nec velit nec lectus imperdiet hendrerit. Donec malesuada nisi augue, id cursus metus tristique ut. Fusce leo ante, convallis id lorem facilisis, luctus lacinia tortor. Praesent pulvinar condimentum massa, in iaculis risus fermentum eget. Integer auctor massa in ultrices ullamcorper. Integer eget porttitor orci. Praesent at est ante. Integer et nibh ex. Suspendisse vehicula diam nec ex laoreet consectetur. Curabitur et tempus odio, at laoreet velit. Morbi in feugiat neque. In eget euismod arcu, at lobortis nisl. Ut ut rutrum elit, ac tristique eros. </p>
-		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rhoncus at nunc a pharetra. Proin tincidunt mi eget blandit efficitur. Suspendisse a odio efficitur, faucibus purus vitae, mollis nisi. Duis quis sem consectetur, scelerisque nisl at, fringilla mauris. Nullam porttitor posuere sollicitudin. Morbi in blandit dolor. Etiam ut pretium risus. </p>
-		<p> Praesent et nibh facilisis, vulputate quam eu, euismod ipsum. Nulla fringilla risus sed ante dapibus rhoncus. Quisque nec pretium est, vitae blandit tortor. Nulla placerat dolor sit amet suscipit interdum. Praesent varius vehicula ante vitae rutrum. Nulla dolor velit, molestie quis nulla ac, laoreet suscipit nisl. Suspendisse arcu tortor, tempus congue velit at, tincidunt laoreet justo. Suspendisse vitae tortor euismod, elementum nulla quis, lobortis nulla. Nulla blandit elit ac est lacinia elementum. Quisque laoreet risus a tortor finibus sollicitudin. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse quis lacinia nibh. Nam eget aliquam justo. Cras pellentesque felis vitae ultrices porta. Praesent ac orci dui. Phasellus nulla lorem, tristique ornare commodo sed, laoreet consectetur nunc. </p>
 		<p> Mauris diam nisi, gravida non porttitor vulputate, volutpat sed odio. Quisque hendrerit, dui eu mattis pharetra, nisi enim tincidunt elit, id dictum justo arcu in lacus. Proin blandit vehicula purus vitae iaculis. In nec velit nec lectus imperdiet hendrerit. Donec malesuada nisi augue, id cursus metus tristique ut. Fusce leo ante, convallis id lorem facilisis, luctus lacinia tortor. Praesent pulvinar condimentum massa, in iaculis risus fermentum eget. Integer auctor massa in ultrices ullamcorper. Integer eget porttitor orci. Praesent at est ante. Integer et nibh ex. Suspendisse vehicula diam nec ex laoreet consectetur. Curabitur et tempus odio, at laoreet velit. Morbi in feugiat neque. In eget euismod arcu, at lobortis nisl. Ut ut rutrum elit, ac tristique eros. </p>
 		<p> Mauris diam nisi, gravida non porttitor vulputate, volutpat sed odio. Quisque hendrerit, dui eu mattis pharetra, nisi enim tincidunt elit, id dictum justo arcu in lacus. Proin blandit vehicula purus vitae iaculis. In nec velit nec lectus imperdiet hendrerit. Donec malesuada nisi augue, id cursus metus tristique ut. Fusce leo ante, convallis id lorem facilisis, luctus lacinia tortor. Praesent pulvinar condimentum massa, in iaculis risus fermentum eget. Integer auctor massa in ultrices ullamcorper. Integer eget porttitor orci. Praesent at est ante. Integer et nibh ex. Suspendisse vehicula diam nec ex laoreet consectetur. Curabitur et tempus odio, at laoreet velit. Morbi in feugiat neque. In eget euismod arcu, at lobortis nisl. Ut ut rutrum elit, ac tristique eros. </p>
 	</div>
@@ -71,5 +65,6 @@
 <!-- FOOTER ==========================================================================================-->
 <?php include '_footer.php' ?>
 <script src = "js/slideshow.js"></script>
+
 
 
