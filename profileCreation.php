70324df361516d37c3ac912726d8b1b8783c4d77
Paul Kwak <kwakp@flip1.engr.oregonstate.edu>
2016-02-18 17:56:13 -0800

    changes to style tings

--- profileCreation.php
+++ profileCreation.php
@@ -1,44 +1,8 @@
 <?php include("_header.php"); ?>
-<style>
-#header {
-    background-color:#6AC6AF;
-    color:white;
-    text-align:center;
-    padding:5px;
-}
-#nav {
-    line-height:30px;
-    background-color:#eeeeee;
-    height:600px;
-    width:100px;
-    float:left;
-    padding:5px; 
-}
-#section {
-    width:950px;
-	height:600px;
-    float:left;
-    padding:10px; 
-}
-#footer {
-    background-color:#6AC6AF;
-    color:white;
-    clear:both;
-    text-align:center;
-    padding:5px; 
-}
-</style>
-<!--colors?
-darker teal #6AC6AF
-tealish#BADFC0
-greyish teal#A7BFB1
--->
-
-<!--<body background="">
--->
 
 
-<body bgcolor="#b3b3b3">
+
+<body>
 	<h1>
 		Creating your Profile
 	</h1>
