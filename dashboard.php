a838b25b7899f4547fb03fd949f5bcca6922c3c5
Shannon Ernst <ernstsh@flip3.engr.oregonstate.edu>
2016-02-28 16:37:37 -0800

    adding dashboard page

---
+++ dashboard.php
@@ -0,0 +1,16 @@
+<?php include("_header.php"); ?>
+<?php
+	if(checkAuth(true) != "") {
+?>
+
+<body bgcolor="#F7A810">
+<h1 style="float: center;">Welcome to Your Hom-E Dashboard</h1>
+
+
+<div style="float: center;">
+	<h1>This is where the dashboard menu should go</h1>
+</div>
+
+</body>
+
+<?php } ?>
