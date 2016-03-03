f2403654bc38e2ef23f782a2a03c307338b76a59
Shannon Ernst <ernstsh@flip2.engr.oregonstate.edu>
2016-03-03 09:37:41 -0800

    Fixed the login issue for dashboard and got the php to work. TODO: need to make the UserID connect to who is logged in

--- dashboard.php
+++ dashboard.php
@@ -27,41 +27,6 @@
     <div id="data" >
 	<h1>Housing data</h1>
 
-		<?php
-				ini_set('display_errors', 'On');
-				$servername = "oniddb.cws.oregonstate.edu";
-				$username = "horvatha-db";
-				$password = "4xunHq7hNuAmTgFb";
-				$dbName = "horvatha-db";
-				$conn = new mysqli($servername, $username, $password, $dbName);
-						 
-						 # dont understand this part
-						if ($result = $conn->query("firstname, lastname, phone, gender, photo, major, age, schYear, relations, bio")) {
-							while($obj = $result->fetch_object()){ 
-							#display photo first
-									echo .htmlspecialchars($obj->photo).;
-									echo "<br>"
-							#display information
-									echo .htmlspecialchars($obj->firstname).; 
-									echo .htmlspecialchars($obj->lastname).; 
-									echo .htmlspecialchars($obj->phone).; 
-									echo .htmlspecialchars($obj->gender).; 
-									echo .htmlspecialchars($obj->major).; 
-									echo .htmlspecialchars($obj->age).; 
-									echo .htmlspecialchars($obj->schYear).; 
-									echo .htmlspecialchars($obj->relations).; 
-									echo "<br>"
-									echo .htmlspecialchars($obj->bio).
-									echo "<br>"
-							} 
-
-						$result->close();
-						}
-
-
-		?>
-		
-</data>
 </div>
 
 
@@ -71,8 +36,40 @@
 	
 	<h1><a href="offerHousing.php">Post House</a></h1>
 	<br>
-	
-	
+	<?php
+				ini_set('display_errors', 'On');
+				$servername = "oniddb.cws.oregonstate.edu";
+				$username = "horvatha-db";
+				$password = "4xunHq7hNuAmTgFb";
+				$dbName = "horvatha-db";
+				$conn = new mysqli($servername, $username, $password, $dbName);
+						 
+						 # dont understand this part
+						 if ($result = $conn->query("SELECT firstName, lastName, phone, Gender, major, age, schYear, relationshipStatus, bio FROM User_T WHERE UserID=1483")) { #NEED TO CHANGE USER ID TO MATCH WITH SOMEONE WHO IS LOGGED IN
+							while($obj = $result->fetch_object()){  
+							#display photo first
+									#echo .htmlspecialchars($obj->photo).;
+									#echo "<br>";
+							#display information
+									echo "<p>".htmlspecialchars($obj->firstName)."</p>"; 
+									echo "<p>".htmlspecialchars($obj->lastName)."</p>"; 
+									echo "<p>".htmlspecialchars($obj->phone)."</p>"; 
+									echo "<p>".htmlspecialchars($obj->Gender)."</p>"; 
+									echo "<p>".htmlspecialchars($obj->major)."</p>"; 
+									echo "<p>".htmlspecialchars($obj->age)."</p>"; 
+									echo "<p>".htmlspecialchars($obj->schYear)."</p>"; 
+									echo "<p>".htmlspecialchars($obj->relationshipStatus)."</p>"; 
+									echo "<br>";
+									echo "<p>".htmlspecialchars($obj->bio)."</p>";
+									echo "<br>";
+							} 
+
+						$result->close();
+						} 
+
+
+		?>
+	</data>
 	<p>London is the capital city of England. It is the most populous city in the United Kingdom,
 	with a metropolitan area of over 13 million inhabitants.</p>
 	<p>Standing on the River Thames, London has been a major settlement for two millennia,
