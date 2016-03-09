83b2f1db728c3fd4fb109f1dfa9520b715718c43
Taylor Acott <taylor22211@gmail.com>
2016-03-08 21:08:11 -0800

    Fixed client validation

--- offeringHousing.php
+++ offeringHousing.php
@@ -25,19 +25,19 @@
 				<input type="radio" name="housingType" value="apartment"> Apartment <br>
 				<input type="radio" name="housingType" value="duplex"> Duplex <br>
 			<label><br>Number of rooms available:</label><br>
-				<input type="text" pattern = "/^[0-9]{1,2}$/" name="roomAmount"><br><br>
+				<input type="text" pattern = "[0-9]{1,2}" title = "Enter 1 or 2 digits" name="roomAmount"><br><br>
 			<label>Address:</label><br>
-				<input type="text" pattern = "/^[a-zA-Z0-9 ]{2,50}$/" name="address"><br><br>
+				<input type="text" pattern = "[a-zA-Z0-9 ]+" title = "Only enter letters, numbers, or spaces" name="address"><br><br>
 			<label>Rent cost per month:</label><br>
-				<input type="text" pattern = "/^[0-9]{1,5}$/" name="costMonth"><br><br>
+				<input type="text" pattern = "[0-9]{1,5}" title = "Enter 1-5 digits" name="costMonth"><br><br>
 			<label>Utility cost per month:</label><br>
-				<input type="text" pattern = "/^[0-9]{1,5}$/" name="costUtil"><br><br>
-			<label>Available move in date:</label><br>
-				<input type="text" pattern = "/^[0-9]{2}-[0-9]{2}-[0-9]{4}$/" name="availableDate"><br><br>
+				<input type="text" pattern = "[0-9]{1,5}" title = "Enter 1-5 digits" name="costUtil"><br><br>
+			<label>Available move in date (DD-MM-YYYYs):</label><br>
+				<input type="text" pattern = "[0-9]{2}-[0-9]{2}-[0-9]{4}" title = "Format: DD-MM-YYYY" name="availableDate"><br><br>
 			<label>Length of lease:</label><br>
-				<input type="text" pattern = "/^[0-9]{1,3}$/" name="leaseLength"><br><br>
+				<input type="text" pattern = "[0-9]{1,3}" title = "Enter 1-3 digits (months)" name="leaseLength"><br><br>
 			<label>Property rules:</label><br>
-				<textarea name="rules" pattern = "/^[a-zA-Z0-9!? ]{1,300}$/" rows="10" cols="30"></textarea>
+				<textarea name="rules" pattern = "[a-zA-Z0-9!? ]{1,300}" title = "Enter at most 300 characters including: letters, numbers, spaces, !, or ?" rows="10" cols="30"></textarea>
 				<br>
 				<br>
 			<input type="submit" name = "submit" value = "Submit">
--- profileCreation.php
+++ profileCreation.php
@@ -19,30 +19,29 @@
 			<legend> Personal information:</legend>
 			
 			First name:<br>
-			<input type="text" pattern "/^[a-zA-Z]+$/" name="firstname"><br>
-			<script> isValid($firstName, $nameRegEx) </script>
+			<input type="text" pattern = "[A-Za-z]+" title = "Only enter letters" name="firstname"><br>
 			Last name:<br>
-			<input type="text" pattern "/^[a-zA-Z]+$/" name="lastname"><br>
+			<input type="text" pattern = "[a-zA-Z]+" title = "Only enter letters" name="lastname"><br>
 			Phone &#40;optional&#41;:<br>
-			<input type="text" pattern = "/^[0-9]{10,11}$/" name="phone"><br><br>
+			<input type="text" pattern = "[0-9]{10,11}" title = "Enter 10 or 11 digits" name="phone"><br><br>
 			Gender:<br>
 				<input type="radio" name="gender" value="male"> Male<br>
 				<input type="radio" name="gender" value="female"> Female<br><br>
 			Major:<br>
-			<input type="text" pattern "/^[a-zA-Z ]{2,30}$/" name="major"><br>
+			<input type="text" pattern = "[a-zA-Z ]+" title = "Only enter letters and spaces" name="major"><br>
 			Age:<br>
-			<input type="text" pattern "/^[0-9]{1,3}$/" name="age"><br>
-			Year in school:<br>
-			<input type="text" pattern "/^[0-9]+$/" name="schYear"><br>
+			<input type="text" pattern = "[0-9]{1,3}" title = "Enter 1-3 digits" name="age"><br>
+			Year in school (as a number):<br>
+			<input type="text" pattern = "[0-9]+" title = "Only enter digits" name="schYear"><br>
 			Relationship Status:<br>
 				<input type="radio" name="relations" value="single"> Single<br>
 				<input type="radio" name="relations" value="dating"> In a Relationship<br>
 				<input type="radio" name="relations" value="prefNotTo"> Prefer not to say<br>
-				<img src="http://www.bizfilings.com/images/QuestionMrk.gif" title="Living conditions can change when you have a partner! Let your potential roommates know!"><br><br>
+				<br>
 			Description/Bio:<br>
-			<textarea name="bio" pattern = "/^[a-zA-Z0-9!?() ]{0,300}$/" rows="6" cols="35"></textarea> 
+			<textarea name="bio" pattern = "[a-zA-Z0-9!? ]{0,300}" title = "Enter at most 300 characters including: letters, numbers, spaces, !, or ?" rows="6" cols="35"></textarea> 
                         <br>ONID Username:<br>
-                        <input type="text" pattern "/^[a-z0-9]+$/" name="ONID"><br><br>
+                        <input type="text" pattern = "[a-z0-9]+" title = "Only enter lowercase letters and digits" name="ONID"><br><br>
 		</fieldset>
 		<fieldset>
 			<legend> Connect through Social:</legend>
